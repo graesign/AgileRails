5029f6709596c6951ff7aae3c1e658489ced4263
JrSchild <joriami@gmail.com>
2014-03-11 12:26:52 +0100

    bugfix and import db.sql

--- Rails7/Code/RailJava/Rails7/src/connection/Dbmanager.java
+++ Rails7/Code/RailJava/Rails7/src/connection/Dbmanager.java
@@ -55,7 +55,7 @@ public class Dbmanager {
      * Executes a query without result.
      * @param query, the SQl query
      */
-    public void executeQuery(String query) {
+    public static void executeQuery(String query) {
         try {
             Statement statement = connection.createStatement();
             statement.executeQuery(query);
@@ -68,7 +68,7 @@ public class Dbmanager {
      * Executes a query with result.
      * @param query, the SQL query
      */
-    public ResultSet doQuery(String query) {
+    public static ResultSet doQuery(String query) {
         ResultSet result = null;
         try {
             Statement statement = connection.createStatement();
@@ -83,7 +83,7 @@ public class Dbmanager {
      * Executes a query with result.
      * @param query, the SQL query
      */
-    public ResultSet insertQuery(String query) {
+    public static ResultSet insertQuery(String query) {
         ResultSet result = null;
         try {
             Statement statement = connection.createStatement();
---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `rails` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `start` int(11) NOT NULL,
+  `end` int(11) NOT NULL,
+  `passengers` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
